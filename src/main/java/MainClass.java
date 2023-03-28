dce1971a1e6949589ca2690b1b227262e43bcf89
zeeshan <zeesarmeen@gmail.com>
2023-03-28 23:32:46 +0530

    change in main class

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -4,6 +4,8 @@ public class MainClass {
 	public static void main(String arg[])
 	{
 		System.out.println("hello i am here ");
+		System.out.println("hello i am here ");
+		System.out.println("hello i am here ");
 	}
 
 }
